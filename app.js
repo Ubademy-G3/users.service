65e7e2b81c229b703d41ebb88794cab5bdff34cc
chiabauni <chiabauni@gmail.com>
2021-09-26 18:04:38 -0300

    Agrego Docker

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+ const app = express();
+ 
+ const port = process.env.PORT;
+ 
+ app.get('/', (req, res) => {
+   res.status(200).send({
+     success: 'true',
+     message: 'Docker listo.',
+     version: '1.0.0',
+   });
+ });
+ 
+ app.listen(port);
+ console.log('Aplicacion ejecutando en el puerto:', port);
