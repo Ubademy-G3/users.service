a6c30fba88a5515b7ca27e47129287812fe2d98a
Daniela <daniela.leloutre@gmail.com>
2021-09-29 18:06:08 -0300

    agrego linter

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const express = require('express');
+const express = require("express");
 
- const app = express();
- 
- const port = process.env.PORT;
- 
- app.get('/', (req, res) => {
-   res.status(200).send({
-     success: 'true',
-     message: 'Docker nuuevo listo.',
-     version: '1.0.0',
-   });
- });
- 
- app.listen(port);
- console.log('Aplicacion ejecutando en el puerto:', port);
+const app = express();
+
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.status(200).send({
+    success: "true",
+    message: "Docker nuuevo listo.",
+    version: "1.0.0",
+  });
+});
+
+app.listen(port);
+// console.log("Aplicacion ejecutando en el puerto:", port);
