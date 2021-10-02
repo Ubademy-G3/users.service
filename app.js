ea3f36198345abbd8a24951f1e304ef577a7b9bd
chiabauni <chiabauni@gmail.com>
2021-10-02 11:33:06 -0300

    Cambio app.js

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-const express = require("express");
+const express = require('express');
+const { Client } = require('pg');
 
 const app = express();
 
-const port = process.env.PORT;
-
-app.get("/", (req, res) => {
-  res.status(200).send({
-    success: "true",
-    message: "Docker nuuevo listo.",
-    version: "1.0.0",
-  });
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  query_timeout: 1000,
+  statement_timeout: 1000
 });
 
-app.listen(port);
-// console.log("Aplicacion ejecutando en el puerto:", port);
+client.connect();
+
+app.get('/ping', (req, res) => res.send('Pong!'));
+
+app.get('/status', (req, res) => 
+    client.query('SELECT NOW()', (err) => res.send({ service: 'UP', db: err ? 'DOWN' : 'UP' }))
+);
+
+app.listen(process.env.PORT, () => {
+    console.log(`App running on port ${process.env.PORT}`); 
+});
