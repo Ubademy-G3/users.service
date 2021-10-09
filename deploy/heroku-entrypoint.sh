4cae8c4711ca8559da13b7088b022921451be0d0
Daniela <daniela.leloutre@gmail.com>
2021-10-09 16:48:29 -0300

    agrego db a entrypoint

--- deploy/heroku-entrypoint.sh
+++ deploy/heroku-entrypoint.sh
@@ -3,4 +3,12 @@
 datadog-agent run > /dev/null &
 /opt/datadog-agent/embedded/bin/trace-agent --config=/etc/datadog-agent/datadog.yaml > /dev/null &
 /opt/datadog-agent/embedded/bin/process-agent --config=/etc/datadog-agent/datadog.yaml > /dev/null &
+
+until PGPASSWORD=$POSTGRES_PASSWORD psql $DATABASE_URL -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - executing command"
+
 npm run start
