d0bb519c9d2af7b131533caf87990cbe2ca62a91
MauRodriguez <mrodriguezb@fi.uba.ar>
2021-10-09 18:09:06 -0300

    prueba

--- deploy/heroku-entrypoint.sh
+++ deploy/heroku-entrypoint.sh
@@ -4,4 +4,4 @@ datadog-agent run > /dev/null &
 /opt/datadog-agent/embedded/bin/trace-agent --config=/etc/datadog-agent/datadog.yaml > /dev/null &
 /opt/datadog-agent/embedded/bin/process-agent --config=/etc/datadog-agent/datadog.yaml > /dev/null &
 
-npm run start
+npm run start --port ${PORT:-3000}
