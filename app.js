8f84789deac9eafbc8b737a2535f70b925df9546
Daniela <daniela.leloutre@gmail.com>
2021-10-18 10:53:11 -0300

    fix lint

--- app.js
+++ app.js
@@ -1,44 +1,41 @@
-const express = require('express');
-const { Client } = require('pg');
-var swaggerUi = require('swagger-ui-express'),
-swaggerDocument = require('./swagger.json');
+const express = require("express");
+const { Client } = require("pg");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 const app = express();
 
-const users = require('./infrastructure/routes/users.js');
+const users = require("./infrastructure/routes/users");
 
-var client;
-if (process.env.NODE_ENV !== 'stage') {
-    client = new Client({
-      connectionString: process.env.DATABASE_URL,
-      query_timeout: 1000,
-      statement_timeout: 1000,
-      ssl: false
-    }); 
+let client;
+if (process.env.NODE_ENV !== "stage") {
+  client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    query_timeout: 1000,
+    statement_timeout: 1000,
+    ssl: false,
+  });
 } else {
-    client = new Client({
-      connectionString: process.env.DATABASE_URL,
-      query_timeout: 1000,
-      statement_timeout: 1000,
-      ssl: {
-        rejectUnauthorized: false
-      }
-    });
+  client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    query_timeout: 1000,
+    statement_timeout: 1000,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
 }
 
 client.connect();
 
-app.use('/users', users);
+app.use("/users", users);
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.get('/ping', (req, res) => res.send('Pong!'));
+app.get("/ping", (req, res) => res.send("Pong!"));
 
-app.get('/status', (req, res) => 
-    client.query('SELECT NOW()', (err) => res.send({ service: 'UP', db: err ? 'DOWN' : 'UP' }))
-);
+app.get("/status", (req, res) => client.query("SELECT NOW()", (err) => res.send({ service: "UP", db: err ? "DOWN" : "UP" })));
 
 app.listen(process.env.PORT, () => {
-    console.log(`App running on port ${process.env.PORT}`); 
+  // console.log(`App running on port ${process.env.PORT}`);
 });
-
--- infrastructure/routes/users.js
+++ infrastructure/routes/users.js
@@ -1,16 +1,17 @@
-//const UsersController = require('../controllers/UsersController');
+// const UsersController = require('../controllers/UsersController');
 
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+
+const router = express.Router();
 
 // Home page route
-router.get('/', function(req, res) {
-  res.send('Página de inicio1');
+router.get("/", (req, res) => {
+  res.send("Página de inicio1");
 });
 
 // Id page route
-router.get('/:id', function(req, res) {
-  res.send('Acerca de esta wiki');
+router.get("/:id", (req, res) => {
+  res.send("Acerca de esta wiki");
 });
 
 module.exports = router;
